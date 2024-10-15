ae8b21b9694366b5bed802a0e9a74290fdeee6e8
duydlm <duydlm@vng.com.vn>
2024-10-15 10:28:50 +0700

    quick_sort

---
+++ quick_sort.py
@@ -0,0 +1,60 @@
+def swap(my_list, index1, index2):
+    temp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = temp
+
+
+def pivot(my_list, pivot_index, end_index):
+    swap_index = pivot_index
+
+    for i in range(pivot_index+1, end_index+1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
+            swap(my_list, swap_index, i)
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
+
+
+def quick_sort_helper(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort_helper(my_list, left, pivot_index-1)
+        quick_sort_helper(my_list, pivot_index+1, right)
+    return my_list
+    
+
+def quick_sort(my_list):
+    quick_sort_helper(my_list, 0, len(my_list)-1)
+
+
+
+
+my_list = [4,6,1,7,3,2,5]
+
+print('List before running pivot():')
+print(my_list)
+
+returned_pivot_index = pivot(my_list, 0, 6)
+
+print('\nList after running pivot():')
+print(my_list)
+
+print('\nReturned Swap Index:')
+print(returned_pivot_index)
+
+quick_sort(my_list)
+print(my_list)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    List before running pivot():
+    [4, 6, 1, 7, 3, 2, 5]
+
+    List after running pivot():
+    [2, 1, 3, 4, 6, 7, 5]
+
+    Returned Swap Index:
+    3
+
+ """
